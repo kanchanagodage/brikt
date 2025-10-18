2f59ca4b94ee78b5582ae5048db4878b43b0fc31
kanchanagodage <kanchanagodage@email.com>
2025-10-18 03:03:54 +0100

    Video Hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,44 +6,52 @@ import { ReactTyped } from "react-typed";
 
 export default function Hero() {
   return (
-    <section className="">
+    <section className="h-full">
       {/* Background Image */}
+      <video
+        className="absolute inset-0 h-full w-full object-cover -z-10"
+        src="/hero-video.webm" // or .webm
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{ width: "100%", height: "auto" }}
+      />
       <div className="relative h-screen max-w-7xl flex items-center justify-center">
-      <Image
+        {/* <Image
         src="/hero-bg-3.jpg" // Put your image inside /public/
         alt="Brikt Background"
         fill
         priority
         className="object-cover fixed inset-0 -z-10"
-      />
+      /> */}
 
-      {/* Overlay for contrast */}
-      <div className="absolute inset-0 bg-black/20 -z-0" />
+        {/* Overlay for contrast */}
+        <div className="absolute inset-0 bg-black/20 -z-0" />
 
-      {/* Content */}
-{/* Content */}
-      <motion.div
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
-        className="relative z-10 text-center px-4"
-      >
-        <h1 className="text-4xl md:text-5xl font-semibold text-white mb-4">
-          Your Partner In{" "}
-          <ReactTyped
-            strings={["Design", "Technology", "Growth"]}
-            typeSpeed={80}
-            backSpeed={50}
-            backDelay={1500}
-            loop
-          />
-        </h1>
+        {/* Content */}
+        {/* Content */}
+        <motion.div
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          className="relative z-10 text-center px-4"
+        >
+          <h1 className="text-4xl md:text-5xl font-semibold text-white mb-4">
+            Your Partner In{" "}
+            <ReactTyped
+              strings={["Design", "Technology", "Growth"]}
+              typeSpeed={80}
+              backSpeed={50}
+              backDelay={1500}
+              loop
+            />
+          </h1>
 
-        <p className="text-base md:text-2xl text-gray-200 max-w-2xl mx-auto">
-          Building bold digital experiences with design, strategy, and tech.
-        </p>
-      </motion.div>
-
+          <p className="text-base md:text-2xl text-gray-200 max-w-2xl mx-auto">
+            Building bold digital experiences with design, strategy, and tech.
+          </p>
+        </motion.div>
       </div>
     </section>
   );
